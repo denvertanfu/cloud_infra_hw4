9f17f31dcdbe27871232e59d235d5d9d49bb2840
denvertanfu <60454133+denvertanfu@users.noreply.github.com>
2021-11-12 09:46:42 -0500

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,10 @@
+import sys
+
+quality_check = [0, 1, 4, 5, 9]
+
+for line in sys.stdin:
+    line = line.strip()
+    temperature = int(line[87:92])
+    quality = int(line[92])
+    if ((temperature != 9999) and (quality in quality_check)):
+        print('%s\t%d' % (line[15:23], int(line[87:92])))
